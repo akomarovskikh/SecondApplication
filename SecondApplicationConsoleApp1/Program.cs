cdb702cee28267ff5bba0d32d0304b8905c8c377
akomarovskikh <alexander.komarovskikh@gmail.com>
2022-03-30 23:47:43 +0300

    Добавьте файлы проекта.

---
+++ SecondApplicationConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SecondApplicationConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myName;
+            myName = "Alexander";
+            Console.WriteLine(myName);
+            Console.ReadKey();
+        }
+    }
+}
