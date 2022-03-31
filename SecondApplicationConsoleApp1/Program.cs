72b2e420aa76b9c156b7dcf4497df7705c348519
akomarovskikh <alexander.komarovskikh@gmail.com>
2022-03-31 23:02:53 +0300

    Добавил в файл program.cs Задание 3.5.3. Вывод в одной строке многих переменных

--- SecondApplicationConsoleApp1/Program.cs
+++ SecondApplicationConsoleApp1/Program.cs
@@ -18,6 +18,21 @@ class MainClass
         Console.WriteLine();
         Console.WriteLine($"Меня зовут {MyNameSecond}");
 
+        string MyName = "Евгения";
+        Console.WriteLine("Привет, мир");
+        Console.WriteLine();
+        Console.WriteLine("Меня зовут {0}", MyName);
+
+        //0 — позиция нашей переменной. То есть переменная будет на позиции 0.
+
+        //Также мы можем вставить в строку не переменную, а строковый литерал:
+        Console.WriteLine("Меня зовут {0}", "Маша");
+
+        //И, естественно, мы можем использовать несколько переменных при выводе. В этом случае располагаем их в порядке нумерации «мест».
+        var age = 27;
+        var weight = 50;
+        Console.WriteLine("Возраст {0} \n Вес {1}", age, weight);
+
         Console.ReadKey();
     }
 }
