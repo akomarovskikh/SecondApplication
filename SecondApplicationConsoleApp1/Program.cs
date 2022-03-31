368270edf538d1a473efb8507f940eb143cc16ce
akomarovskikh <alexander.komarovskikh@gmail.com>
2022-03-31 22:47:22 +0300

    Использовал интерполяцию в файле program.cs

--- SecondApplicationConsoleApp1/Program.cs
+++ SecondApplicationConsoleApp1/Program.cs
@@ -3,16 +3,21 @@ class MainClass
 {
     public static void Main(string[] args)
     {
-
-        string MyName = "Jane";
+        string MyNameOne = "Jane";
         byte MyAge = 27;
         bool HaveIApet = true;
         double MyShoeSize = 37.5;
 
-        Console.WriteLine("My name is " + MyName);
+        Console.WriteLine("My name is " + MyNameOne);
         Console.WriteLine("MyAge " + MyAge);
         Console.WriteLine("Do I have a pet? " + HaveIApet);
         Console.WriteLine("My shoe size is " + MyShoeSize);
 
+        string MyNameSecond = "Евгения";
+        Console.WriteLine("Привет, Мир");
+        Console.WriteLine();
+        Console.WriteLine($"Меня зовут {MyNameSecond}");
+
+        Console.ReadKey();
     }
 }
